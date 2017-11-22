a37ddf226d99f9d47bc4c8473eff8e32a3b0bf24
bzick <a.cobest@gmail.com>
2017-11-23 01:25:48 +0300

    add event wrapper

--- src/PHPLibEventFallback.php
+++ src/PHPLibEventFallback.php
@@ -6,13 +6,15 @@ namespace ION\PHPLibEventFallback;
 
 use ION\PHPLibEventFallback\Wrapper\EventBaseWrapper;
 use ION\PHPLibEventFallback\Wrapper\EventBufferWrapper;
+use ION\PHPLibEventFallback\Wrapper\EventWrapper;
 
 class PHPLibEventFallback
 {
 
     public static function setUp()
     {
-        stream_wrapper_register(EventBaseWrapper::WRAPPER_NAME, EventBaseWrapper::class);
+        stream_wrapper_register(EventBaseWrapper::WRAPPER_NAME,   EventBaseWrapper::class);
+        stream_wrapper_register(EventWrapper::WRAPPER_NAME,       EventWrapper::class);
         stream_wrapper_register(EventBufferWrapper::WRAPPER_NAME, EventBufferWrapper::class);
     }
 }
---
+++ src/Wrapper/EventWrapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ */
+
+namespace ION\PHPLibEventFallback\Wrapper;
+
+
+class EventWrapper
+{
+    const WRAPPER_NAME = "ion.plf.event";
+
+    public $context;
+}
